f0edcf75277c4478585cb9c926cb2ad9a59e59d0
jeremiasolivera <jeremiasolivera166@gmail.com>
2023-07-18 00:59:08 -0300

    first commit

---
+++ src/helpers/index.js
@@ -0,0 +1,7 @@
+let cursor = document.querySelector('#cursor')
+      document.addEventListener('mousemove', function(e){
+        let x = e.clientX;
+        let y = e.clientY;
+        cursor.style.left = x + 'px'
+        cursor.style.top = y + 'px'
+      })
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  fontFamily: {
+			custom: ['Inter:wght', 'sans-serif'],
+		  },
+		  colors:{
+			'azul' : {
+			  100: '#0F172A',
+			  200: '#192b51',
+			  300: '#233f7a',
+			  400: '#2850a2',
+			},
+		  },
+		},
+	  },
+	  plugins: [],
+}
